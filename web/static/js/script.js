a8a057ae52e1047f4dbb03d5cbe0027b9cc8e365
MarcosNBJ <marcosnery.comp@gmail.com>
2020-12-05 23:17:08 -0300

    Refactoring frontend to new project

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -18,11 +18,10 @@ $(function () {
 
     $("#register").click(function () {
 
-        var user_name = $("#user-name").val();
-        var user_contact = $("#user-contact").val();
+        var user_letterbox = $("#user_letterbox").val();
 
-        if (!user_name || !user_contact) {
-            var failure = '<div id="failure" class="ml4 alert alert-danger"><p>Name and Contact Info cannot be empty</p></div>'
+        if (!user_letterbox) {
+            var failure = '<div id="failure" class="ml4 alert alert-danger"><p>Letterbox username cannot be empty</p></div>'
             $(failure).hide().prependTo('#genres').fadeIn();
             setTimeout(function () {
                 $('#failure').fadeOut();
@@ -40,9 +39,8 @@ $(function () {
             type: "POST",
             contentType: 'application/json',
             data: JSON.stringify({
-                "user_name": user_name,
-                "user_preference": user_preference,
-                "user_contact": user_contact
+                "user_letterbox": user_letterbox,
+                "user_preference": user_preference
             }),
             success: function () {
                 var success = '<div id="success" class="ml4 alert alert-success"><p>Preferences recorded!</p></div>'
@@ -60,7 +58,7 @@ $(function () {
 
         $('#tutorial-text').fadeOut(300, function(){ $(this).remove();});
 
-        var user_name = $("#user-name").val();
+        var user_letterbox = $("#user_letterbox").val();
 
         if (user_preference.length == 0) {
             sortable('.js-sortable', 'serialize')[0].items.forEach(element => { user_preference.push(element.node.id) });
@@ -71,7 +69,7 @@ $(function () {
             type: "POST",
             contentType: 'application/json',
             data: JSON.stringify({
-                "user_name": user_name,
+                "user_letterbox": user_letterbox,
                 "user_preference": user_preference
             }),
             success: function (response) {
